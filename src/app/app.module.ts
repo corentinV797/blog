e5a89a6aec72ee0f225120576a546de8cd3ca760
Corentin Vives <corentin.vives@viseo.com>
2021-03-23 12:43:14 +0100

    [feat] postlist.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostlistComponent } from './postlist/postlist.component';
 import { PostlistitemComponent } from './postlistitem/postlistitem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostlistComponent,
     PostlistitemComponent
   ],
   imports: [
---
+++ src/app/postlist/postlist.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import Post from '../post.model';
+
+
+@Component({
+  selector: 'app-postlist',
+  templateUrl: './postlist.component.html',
+  styleUrls: ['./postlist.component.scss']
+})
+export class PostlistComponent implements OnInit {
+  @Input() posts: Post[];
+
+  constructor() {
+    this.posts = [];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
