5be92ff55550c407f7114fb647d0efa0ad5630d0
Corentin Vives <corentin.vives@viseo.com>
2021-04-02 17:13:38 +0200

    [feat] postlist.component: postSubscription

--- src/app/postlist/postlist.component.ts
+++ src/app/postlist/postlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import Post from '../post.model';
 import { PostsService } from '../services/posts.service';
 
@@ -9,13 +10,22 @@ import { PostsService } from '../services/posts.service';
   styleUrls: ['./postlist.component.scss']
 })
 export class PostlistComponent implements OnInit {
-  posts: Post[];
+  posts: Post[] = [];
+  postSubscription: Subscription = Subscription.EMPTY;
 
-  constructor(postsService: PostsService) {
-    this.posts = postsService.getPosts();
+  constructor(private postsService: PostsService) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.postSubscription = this.postsService.postsSubject.subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+    this.postsService.emitPosts();
   }
 
+  ngOnDestroy() {
+    this.postSubscription.unsubscribe();
+  }
 }
