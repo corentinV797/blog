c4b14b2b859b7684adfcaae2d55f26a9d6038dd4
Corentin Vives <corentin.vives@viseo.com>
2021-04-02 17:58:41 +0200

    [feat] first routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostlistComponent } from './postlist/postlist.component';
 import { PostlistitemComponent } from './postlistitem/postlistitem.component';
 import { PostsService } from './services/posts.service';
+
+const appRoutes: Routes = [
+  { path: 'posts', component: PostlistComponent },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: '**', redirectTo: 'posts' }
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +22,8 @@ import { PostsService } from './services/posts.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [PostsService],
   bootstrap: [AppComponent]
